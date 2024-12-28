d1108ac3cc945c4e0b949409a5d40436936d791e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-12-28 15:54:27 +0300

    Added solution for task: Задание: Реализация CRUD для задач

--- 05-db/01-crud-tasks/tasks/dto/create-task.dto.ts
+++ 05-db/01-crud-tasks/tasks/dto/create-task.dto.ts
@@ -1,1 +1,9 @@
-export class CreateTaskDto {}
+import { IsString } from "class-validator";
+
+export class CreateTaskDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+}
--- 05-db/01-crud-tasks/tasks/tasks.service.ts
+++ 05-db/01-crud-tasks/tasks/tasks.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Task } from "./entities/task.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {}
+  constructor(
+    @InjectRepository(Task) private readonly tasksRepository: Repository<Task>,
+  ) {}
 
-  async findAll() {}
+  create(createTaskDto: CreateTaskDto) {
+    const task = this.tasksRepository.create(createTaskDto);
+    return this.tasksRepository.save(task);
+  }
 
-  async findOne(id: number) {}
+  async findAll() {
+    return await this.tasksRepository.find();
+  }
 
-  async update(id: number, updateTaskDto: UpdateTaskDto) {}
+  async findOne(id: number) {
+    const task = await this.tasksRepository.findOneBy({ id });
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    return task;
+  }
 
-  async remove(id: number): Promise<void> {}
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    const task = await this.findOne(id);
+    Object.assign(task, updateTaskDto);
+    return await this.tasksRepository.save(task);
+  }
+
+  async remove(id: number): Promise<void> {
+    const task = await this.findOne(id);
+    await this.tasksRepository.remove(task);
+  }
 }
